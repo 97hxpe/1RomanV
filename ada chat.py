11e2aa8883aa2c3ac60e8e757cd58409a1d8973c
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-04 14:51:20 +0200

    Add files via upload

---
+++ ada chat.py
@@ -0,0 +1,19 @@
+name = input("Wie ist dein Name? ")
+print("Hallo, " + name)
+antwort = input("schöner name, mein name ist ada")
+freizeit = input("was machst du in deiner freizeit? ")
+print("voll cool")
+age = input("Wie alt bist du? ")
+print("oha echt schon " + age)
+question = input("Hast du Haustiere ")
+if question.lower() == 'ja':
+    print("wie cool")
+if question.lower() == 'nein':
+    print("schade")
+question = input("Magst du Burger oder Pizza mehr ")
+if question.lower() == 'burger':
+    print("Ich auch")
+if question.lower() == 'pizza':
+ print("Ich mag Burger mehr")
+
+
