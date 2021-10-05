ff072332e50e86036bd793f3602c197ca2c0bef5
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-05 11:49:44 +0200

    Add files via upload

---
+++ worklifebalance.py
@@ -0,0 +1,18 @@
+import time
+
+print("hallo")
+
+question = input("willst du ein kleines warmup zum denken \n")
+if question.lower() == 'ja':
+    import random
+for i in range(1):
+    print(random.randint(1,6)) 
+ 
+def wuerfeln():
+    return(random.randint(1,6))
+wuerfeln()
+
+
+
+if question.lower() == 'nein':
+ print("schade")
