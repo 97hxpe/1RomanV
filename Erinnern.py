efc776aaabefafefdc200667935d49845d087d1c
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-05 11:36:22 +0200

    Add files via upload

---
+++ Erinnern.py
@@ -0,0 +1,8 @@
+import time
+
+zeit = input("Wann willst du erinnert werden")
+print("jetzt eine pause du meintest " + zeit)
+time = input("du wirst bald erinnert wann genau")
+print(" ich erinnere dich in " + time)
+time("0+time")
+print("fertig+time")
