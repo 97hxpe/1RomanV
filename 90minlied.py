8b91592d2c9e5c4c09ebcc14c9cb355086681280
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-05 11:21:03 +0200

    Add files via upload

---
+++ 90minlied.py
@@ -0,0 +1,6 @@
+import webbrowser
+import time
+print ("jetzt eine kleine pause")
+print("lied wird geladen")
+time.sleep(90)
+webbrowser.open("https://www.youtube.com/watch?v=2KDlDuS6r6I")
---
+++ fragen 10 .py
@@ -0,0 +1,18 @@
+import time
+time.sleep (0)
+print("1. Was war deine erste Arbeitsstelle?")
+print("2. Hast du einmal eine berühmte Person getroffen?")
+print("3. Was liest du gerade?")
+print("4. Wenn du eine neue Fertigkeit in Sekundenschnelle erlernen könntest, was wäre das?")
+print("5. Wer ist jemand, den du wirklich bewunderst?")
+print("6. Hast du irgendwelche Lieblingszitate?")
+print("7. Was ist deine früheste Erinnerung?")
+print("8. Was ist deine liebste Familientradition?")
+print("9. Was war vor 10 Jahren deine Lieblingsband?")
+print("10. Wer hatte den größten Einfluss auf dein Heranwachsen?")
+time.sleep(2)
+
+   
+print("ENDE Hände weg ")
+print("ENDE Hände weg ")
+print("ENDE Hände weg ")
