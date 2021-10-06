fa9e6f39c4095beb86d5fb38e0f7d998928bf770
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-06 11:48:58 +0200

    Delete 30sec timer.py

--- 30sec timer.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-import time
-
-prompt = input("Gebe 'suche' ein, um Google zu öffnen. ")
-if prompt.lower() == 'suche':
-    print("Der Browser wird geöffnet...")
-    time.sleep(0)
-    webbrowser.open("https://www.google.com")
