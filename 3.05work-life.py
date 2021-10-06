a2cb7668c3af62561d5d9bf08e003f0020cfff22
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-06 12:10:21 +0200

    Add files via upload

---
+++ 3.05work-life.py
@@ -0,0 +1,88 @@
+import time
+
+print("hallo")
+
+question = input("willst du ein kleines warmup zum denken \n")
+if question.lower() == 'ja':
+    import random
+import time
+
+
+range = pirat=random.randint(1,100)
+
+
+print("Hallo ich bin Käptn Hook und habe mir eine zahl ausgedacht, wenn du sie errätst, bekommst du deine Beute! du hast 6 Versuche.")
+time.sleep(0.5)
+Zahl=int(input("Errate die Zahl zwischen 1-100"))
+
+
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!\n ")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte! !")
+else: print("zu hoch .")
+
+
+Zahl=int(input("noch 5 Versuche!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!\n ")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte!!")
+else: print("zu hoch.")
+
+
+Zahl=int(input("noch 4 Versuche!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!\n")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte!!")
+else: print("zu hoch .")
+
+
+Zahl=int(input("noch 3 Versuche\n!"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!\n")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte!")
+else: print("zu hoch .")
+Zahl=int(input("noch 2 Versuche Kamerad!\n"))
+
+               
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!\n")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte!!\n")
+else: print("zu hoch.")
+Zahl=int(input("noch 1 Versuch !"))
+
+
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!\n")
+    exit()
+
+
+if Zahl < pirat:
+    print("Zu niedrig, du Landratte!")
+
+    
+else: print("zu hoch.")
+print("Game over")
+exit()
+
+if question.lower() == 'nein':
+    print("schade")
