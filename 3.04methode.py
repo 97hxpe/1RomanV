0a8cf35e25611b8b1137334e53ddb45af26c8fc9
1RomanV <91878139+1RomanV@users.noreply.github.com>
2021-10-06 14:13:31 +0200

    Add files via upload

---
+++ 3.04methode.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+print ("schreibe methode und die sekundenanzahl in klammern um in den gewünschten minuten den song zu höhren")
+def methode(length):
+    time ==("lenght")
+    print("Zeit für eine Pause!")
+    webbrowser.open('https://www.youtube.com/watch?v=6FqlENf1jZc')
+    time ==("length")
+    print("Zeit zum Weiterarbeiten!\n")    
+    print("Ende!")
